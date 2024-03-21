5343bca823b45b7aa34543fe65009490a17170f0
sadafzeeshan9 <160055415+sadafzeeshan9@users.noreply.github.com>
2024-03-21 21:51:30 +0500

    Add files via upload
    i make simple calculator with the help of typescript  and inquirer

---
+++ index.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node              
+import inquirer from "inquirer";
+
+const answers: {
+  firstNumber: number;
+  secondNumber: number;
+  operator: string;
+} = await inquirer.prompt([
+  {
+    type: "number",
+    name: "firstNumber",
+    message: "enter first number",
+  },
+
+  { type: "number", name: "secondNumber", message: "enter first number" },
+  {
+    type: "list",
+    name: "operator",
+    choices: ["addition", "subtraction", "multiplication", "division"],
+    message: "select one of the operator to perform operation",
+  },
+]);
+
+//conditional statment//
+
+if (answers.operator === "addition") {
+  console.log(answers.firstNumber + answers.secondNumber);
+} else if (answers.operator === "subtraction") {
+  console.log(answers.firstNumber - answers.secondNumber);
+} else if (answers.operator === "multiplication") {
+  console.log(answers.firstNumber * answers.secondNumber);
+} else if (answers.operator === "division") {
+  console.log(answers.firstNumber / answers.secondNumber);
+}
+else{
+  console.log("please select")
+}
